31797cdf4068eb9776f9a6e48d683fd78ff85e37
xiaoxuan <xiaoxuan@qq.com>
2021-08-03 15:16:41 +0800

    准备演示数据

--- src/main.js
+++ src/main.js
@@ -10,3 +10,21 @@ app.listen(port, () => {
 app.get('/', (request, response) => {
   response.send('您好');
 });
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜天，春来江水绿如蓝'
+  },
+];
