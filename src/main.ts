4a3acde0709f51eb870f6bf5913d2dc019dcb0a4
xiaoxuan <xiaoxuan@qq.com>
2021-08-04 16:55:27 +0800

    自动排版代码

--- src/main.ts
+++ src/main.ts
@@ -20,17 +20,17 @@ const data = [
   {
     id: 1,
     title: '关山月',
-    content: '明月出天山，苍茫云海间'
+    content: '明月出天山，苍茫云海间',
   },
   {
     id: 2,
     title: '望岳',
-    content: '会当凌绝顶，一览众山小'
+    content: '会当凌绝顶，一览众山小',
   },
   {
     id: 3,
     title: '忆江南',
-    content: '日出江花红胜天，春来江水绿如蓝'
+    content: '日出江花红胜天，春来江水绿如蓝',
   },
 ];
 
@@ -43,7 +43,7 @@ app.get('/posts/:postId', (request: Request, response: Response) => {
   const { postId } = request.params;
 
   // 查找具体内容
-  const posts = data.filter(item => item.id == parseInt(postId, 10));
+  const posts = data.filter((item) => item.id == parseInt(postId, 10));
 
   // 作出响应
   response.send(posts[0]);
@@ -63,10 +63,10 @@ app.post('/posts', (request: Request, response: Response) => {
   console.log(request.headers['sing-along']);
 
   // 设置响应头部数据
-  response.set('Sing-Along', 'How I wonder what you are!')
+  response.set('Sing-Along', 'How I wonder what you are!');
 
   // 作出响应
   response.send({
-    message: `成功创建了内容：${content}`
+    message: `成功创建了内容：${content}`,
   });
 });
