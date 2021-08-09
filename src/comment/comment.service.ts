07af11d00dbdbdf16703e45ad0f12cf9e229a643
xiaoxuan <xiaoxuan@qq.com>
2021-08-09 18:20:19 +0800

    定义存储评论数据的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,1 +1,22 @@
 import { Connection } from "mysql2/typings/mysql";
+import { connection } from "../app/database/mysql";
+import { CommentModel } from './comment.model';
+
+/**
+ * 创建评论
+ */
+export const createComment = async (
+  comment: CommentModel
+) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO comment
+    SET ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, comment);
+
+  // 提供数据
+  return data;
+};
