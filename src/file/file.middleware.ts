2040e8207665b7d1b9883672d9b3e253a122199e
xiaoxuan <xiaoxuan@qq.com>
2021-08-09 15:41:57 +0800

    图像信息：读取图像文件附加信息

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import Jimp from 'jimp';
 import multer from 'multer';
 
 /**
@@ -12,3 +13,29 @@ const fileUpload = multer({
  * 文件拦截器
  */
 export const fileInterceptor = fileUpload.single('file');
+
+/**
+ * 文件处理器
+ */
+ export const fileProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 文件路径
+  const { path } = request.file;
+
+  let image: Jimp;
+
+  try {
+    // 读取图像文件
+    image = await Jimp.read(path);
+  } catch (error) {
+    return next(error);
+  }
+
+  console.log(image);
+
+  // 下一步
+  next();
+};
