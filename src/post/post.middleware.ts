2df1267d994fd99e3888ed64f2f8f620cbb68f2b
xiaoxuan <xiaoxuan@qq.com>
2021-08-10 13:37:50 +0800

    创建内容分页中间件

--- src/post/post.middleware.ts
+++ src/post/post.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, request } from 'express';
 import { POSTS_PER_PAGE } from '../app/app.config';
 
 /**
@@ -72,6 +72,30 @@ import { POSTS_PER_PAGE } from '../app/app.config';
       param: `${user}`,
     };
   }
+
+  // 下一步
+  next();
+};
+
+/**
+ * 内容分页
+ */
+export const paginate = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 当前页码
+  const { page = 1 } = request.query;
+
+  // 每页内容数量
+  const limit = parseInt(POSTS_PER_PAGE, 10) || 30;
+
+  // 计算出偏移量
+  const offset = limit * (parseInt(`${page}`, 10) - 1);
+
+  // 设置请求中的分页
+  request.pagination = { limit, offset };
 
   // 下一步
   next();
