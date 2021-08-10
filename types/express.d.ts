9a622b61cd2cc0355c1d730b0ef49e5c69161438
xiaoxuan <xiaoxuan@qq.com>
2021-08-10 12:53:25 +0800

    创建过滤列表中间件

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,5 +1,6 @@
 import exp from 'constants';
 import { TokenPayload } from '../src/auth/auath.interface';
+import { GetPostsOptionsFilter } from '../src/post/post.service';
 
 declare global {
   namespace Express {
@@ -7,6 +8,7 @@ declare global {
       user: TokenPayload;
       fileMetaData: { width?: number; height?: number; metadata?: {} };
       sort: string;
+      filter: GetPostsOptionsFilter;
     }
   }
 }
