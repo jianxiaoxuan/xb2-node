d1d7e2191376905a6780abb8a9a7fffc99aa66d9
xiaoxuan <xiaoxuan@qq.com>
2021-08-10 12:08:10 +0800

    改造调取内容列表用的功能

--- types/express.d.ts
+++ types/express.d.ts
@@ -6,6 +6,7 @@ declare global {
     export interface Request {
       user: TokenPayload;
       fileMetaData: { width?: number; height?: number; metadata?: {} };
+      sort: string;
     }
   }
 }
