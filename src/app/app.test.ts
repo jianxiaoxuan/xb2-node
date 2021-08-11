76ed277d666c6df54164633621a07538a3d956df
xiaoxuan <xiaoxuan@qq.com>
2021-08-11 15:34:05 +0800

    创建于运行测试

---
+++ src/app/app.test.ts
@@ -0,0 +1,15 @@
+import { greet } from "./playground/demo";
+
+/**
+ * 单元测试
+ */
+describe('演示单元测试', () => {
+  // 测试
+  test('测试 greet 函数', () => {
+    // 准备
+    const greeting = greet('小炫');
+
+    // 断言
+    expect(greeting).toBe('你好, 小炫');
+  });
+});
