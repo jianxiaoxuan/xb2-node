0a6d1110f5a4bcb270a7e9b540d6a368e949bdce
xiaoxuan <xiaoxuan@qq.com>
2021-08-11 10:10:55 +0800

    定义按ID调取内容的功能

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -79,6 +79,10 @@ export const defaultErrorHandler = (
       statusCode = 400;
       message = '不能上传此类型文件';
       break;
+    case 'NOT_FOUND':
+      statusCode = 404;
+      message = '没找到 ～～ 🦖';
+      break;
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ～～ 🌲';
